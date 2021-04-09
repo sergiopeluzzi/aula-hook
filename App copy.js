4b0ff4b8c9286502425506d7ac6465c81300dd4a
Sergio Peluzzi <sergiopeluzzi@gmail.com>
2021-04-09 19:57:47 -0400

    Meu primeiro commit

---
+++ App copy.js
@@ -0,0 +1,38 @@
+import { StatusBar } from 'expo-status-bar';
+import React, { Component } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
+
+//APP como class
+export default class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      count: 0
+    }
+  }
+  
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Você clicou { this.state.count }  </Text>
+        <Button 
+          title="Clique aqui!"
+          onPress={() => {
+              this.setState({ count: this.state.count + 1 })
+            }
+          }
+        />
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
--- App.js
+++ App.js
@@ -1,11 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Animated, Button, StyleSheet, Text, View } from 'react-native';
 
+//APP como function
 export default function App() {
+
+  const [count, setCount] = useState(0)
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Você clicou { count } vezes </Text>
+      <Button 
+        title="Clique aqui!"
+        onPress={() => {
+            setCount(count + 1)
+          }
+        }
+      />
       <StatusBar style="auto" />
     </View>
   );
